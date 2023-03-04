999b78fd85fb887960b842f35d4886905557d68c
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-03-04 13:00:44 +0530

    Added: Program to add a new contact to addressbook

--- src/main/java/com/bridgelabz/address_book_system/AddressBookMain.java
+++ src/main/java/com/bridgelabz/address_book_system/AddressBookMain.java
@@ -4,5 +4,7 @@ package com.bridgelabz.address_book_system;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to address book program");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContact();
     }
 }
