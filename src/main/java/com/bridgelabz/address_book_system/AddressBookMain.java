ab15c5b6b23f1ad8a9e1abd7680e28d21c7e53a1
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-03-04 12:44:31 +0530

    Added: Program to print welcome message

---
+++ src/main/java/com/bridgelabz/address_book_system/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.address_book_system;
+
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to address book program");
+    }
+}
